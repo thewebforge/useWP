6759bd92d7cb81998c2db4340adc45309cf3d16c
Cédric Bontems <67456035+cbontems@users.noreply.github.com>
2024-04-03 09:55:50 +0200

    fix(darkmode): apply to root element and optional iframe

--- hooks/useDarkMode.ts
+++ hooks/useDarkMode.ts
@@ -2,14 +2,22 @@ import { useEffect } from "@wordpress/element";
 import useMediaQuery from "./useMediaQuery.js"
 import { useLocalStorage } from "./useStorage.js"
 
-export default function useDarkMode(key: string) {
+export default function useDarkMode(key: string, iframeSelector: string | null = null) {
   const [darkMode, setDarkMode] = useLocalStorage(key, true)
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
   const enabled = darkMode ?? prefersDarkMode
 
   useEffect(() => {
-    const root = document.body;
-    root?.setAttribute("data-theme", enabled ? "dark" : "light");
+    document.firstElementChild?.setAttribute(
+      "data-theme",
+      enabled ? "dark" : "light"
+    );
+    if (iframeSelector) (document
+      .getElementById(iframeSelector) as HTMLIFrameElement)
+      ?.contentDocument?.firstElementChild?.setAttribute(
+        "data-theme",
+        enabled ? "dark" : "light"
+      );
   }, [enabled])
 
   return [enabled, setDarkMode]
